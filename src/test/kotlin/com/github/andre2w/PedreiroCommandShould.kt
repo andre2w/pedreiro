390fdd80c8da00f4a5509b6ba7fb0de4b83395ac
Andre Torres <andre855@gmail.com>
2020-07-13 22:57:33 +0100

    Add tests for ExecuteCommand and fix tests for PedreiroCommand

--- src/test/kotlin/com/github/andre2w/PedreiroCommandShould.kt
+++ src/test/kotlin/com/github/andre2w/PedreiroCommandShould.kt
@@ -2,6 +2,7 @@ package com.github.andre2w
 
 import com.github.andre2w.pedreiro.Arguments
 import com.github.andre2w.pedreiro.Pedreiro
+import com.github.andre2w.pedreiro.environment.ConsoleHandler
 import io.micronaut.configuration.picocli.PicocliRunner
 import io.micronaut.context.ApplicationContext
 import io.micronaut.context.env.Environment
@@ -9,18 +10,19 @@ import io.mockk.mockk
 import io.mockk.verify
 import org.junit.jupiter.api.Test
 
-class PedreiroCommandTest {
+
+class PedreiroCommandShould {
 
     @Test
     fun testWithCommandLineOption() {
         val ctx = ApplicationContext.run(Environment.CLI, Environment.TEST)
         val pedreiro = mockk<Pedreiro>(relaxUnitFun = true)
-
+        val consoleHandler = mockk<ConsoleHandler>(relaxed = true)
         ctx.registerSingleton(pedreiro)
+        ctx.registerSingleton(consoleHandler)
+
         val args = arrayOf("testBlueprint", "--arg", "test=blueprint", "--arg", "other=field", "--arg", "other=field")
         PicocliRunner.run(PedreiroCommand::class.java, ctx, *args)
-
-        ctx.close()
 
         verify {
             pedreiro.build(Arguments("testBlueprint", mapOf(
@@ -28,6 +30,7 @@ class PedreiroCommandTest {
                     "other" to "field"
             )))
         }
+        ctx.close()
     }
 
 
---
+++ src/test/kotlin/com/github/andre2w/pedreiro/tasks/ExecuteCommandShould.kt
@@ -0,0 +1,35 @@
+package com.github.andre2w.pedreiro.tasks
+
+import com.github.andre2w.pedreiro.environment.LocalEnvironment
+import com.github.andre2w.pedreiro.environment.ProcessExecutor
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Test
+
+class ExecuteCommandShould {
+
+    private val processExecutor = mockk<ProcessExecutor>(relaxUnitFun = true)
+    private val runFolder = "/home/andre/projects"
+    private val command = "gradle wrapper"
+
+    @Test
+    fun `execute command in the proper folder`() {
+        val environment = mockk<LocalEnvironment>()
+        every { processExecutor.execute(command, "$runFolder/test-project") } returns 0
+        every { environment.currentDir() } returns runFolder
+
+        val executeCommand = ExecuteCommand(
+                "gradle wrapper",
+                "test-project",
+                processExecutor,
+                environment
+        )
+        executeCommand.execute()
+
+        verify {
+            processExecutor.execute(command, "$runFolder/test-project")
+        }
+    }
+
+}
