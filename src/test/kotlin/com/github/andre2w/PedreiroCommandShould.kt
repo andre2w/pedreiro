61ecf660fa51130cd2d5f51aab61551ac4f21e63
Andre Torres <andre855@gmail.com>
2020-07-14 23:26:14 +0100

    Add unit test for unhappy path in PedreiroCommand

--- src/test/kotlin/com/github/andre2w/PedreiroCommandShould.kt
+++ src/test/kotlin/com/github/andre2w/PedreiroCommandShould.kt
@@ -2,35 +2,71 @@ package com.github.andre2w
 
 import com.github.andre2w.pedreiro.Arguments
 import com.github.andre2w.pedreiro.Pedreiro
+import com.github.andre2w.pedreiro.blueprints.BlueprintParsingException
 import com.github.andre2w.pedreiro.environment.ConsoleHandler
 import io.micronaut.configuration.picocli.PicocliRunner
 import io.micronaut.context.ApplicationContext
 import io.micronaut.context.env.Environment
+import io.mockk.clearAllMocks
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 
 class PedreiroCommandShould {
 
+    private val pedreiro = mockk<Pedreiro>(relaxUnitFun = true)
+    private val consoleHandler = mockk<ConsoleHandler>(relaxed = true)
+
+    @BeforeEach
+    internal fun setUp() {
+        clearAllMocks()
+    }
+
     @Test
-    fun testWithCommandLineOption() {
-        val ctx = ApplicationContext.run(Environment.CLI, Environment.TEST)
-        val pedreiro = mockk<Pedreiro>(relaxUnitFun = true)
-        val consoleHandler = mockk<ConsoleHandler>(relaxed = true)
-        ctx.registerSingleton(pedreiro)
-        ctx.registerSingleton(consoleHandler)
+    fun `run Pedreiro application with arguments`() {
+        val ctx = newPedreiroContext()
 
         val args = arrayOf("testBlueprint", "--arg", "test=blueprint", "--arg", "other=field", "--arg", "other=field")
         PicocliRunner.run(PedreiroCommand::class.java, ctx, *args)
 
         verify {
-            pedreiro.build(Arguments("testBlueprint", mapOf(
-                    "test" to "blueprint",
-                    "other" to "field"
-            )))
+            pedreiro.build(parsedArguments())
         }
         ctx.close()
+    }
+
+    @Test
+    internal fun `print message and exit with code 1 in case of an error`() {
+        val ctx = newPedreiroContext()
+        val args = arrayOf("testBlueprint", "--arg", "test=blueprint", "--arg", "other=field", "--arg", "other=field")
+        val parsedArguments = parsedArguments()
+        val errorMessage = "Failed to parse blueprint"
+        every { pedreiro.build(parsedArguments) } throws BlueprintParsingException(errorMessage)
+
+        PicocliRunner.run(PedreiroCommand::class.java, ctx, *args)
+
+        verify {
+            consoleHandler.exitWith(1)
+            consoleHandler.print(errorMessage)
+        }
+        ctx.close()
+    }
+
+    private fun parsedArguments(): Arguments {
+        return Arguments("testBlueprint", mapOf(
+                "test" to "blueprint",
+                "other" to "field"
+        ))
+    }
+
+    private fun newPedreiroContext(): ApplicationContext {
+        val ctx = ApplicationContext.run(Environment.CLI, Environment.TEST)
+        ctx.registerSingleton(pedreiro)
+        ctx.registerSingleton(consoleHandler)
+        return ctx
     }
 
 
